ce9d9fa2e43e0420a566f25f29f9eea0ca1e76c0
ap-ahmad-nabil <ahmad.nabil@astrapay.com>
2024-02-18 20:45:20 +0700

    add search feature

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,56 @@
+import UserCard from "@/components/cards/UserCard";
+import PostThread from "@/components/forms/PostThread";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constant";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+async function Page({ params }: { params: { id: string } }) {
+    const user = await currentUser();
+    if(!user) return null;
+
+    // const userInfo = await fetchUser(params.id);
+    // console.log(userInfo);
+    // if(!userInfo?.onboarded) return redirect('/onboarding');
+
+    // Fetch user 
+    
+    
+    const result = await fetchUsers({
+        userId: user.id,
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25
+    })
+  return (
+    <section>
+        <h1 className="head-text mb-10">Search</h1>
+        {/* Search Bar */}
+
+        <div className="mt-14 flex flex-col gap-9">
+            {result.users.length === 0 ? (
+                <p className="no-result"No Users></p>
+            ): (
+                <>
+                    {result.users.map((person) => (
+                        <UserCard 
+                            key={person.id}
+                            id={person.id}
+                            name={person.name}
+                            username={person.username}
+                            imgUrl={person.image}
+                            personType='User'
+                        />
+                    ))}
+                </>
+            )}
+        </div>
+    </section>
+  )
+}
+
+export default Page
